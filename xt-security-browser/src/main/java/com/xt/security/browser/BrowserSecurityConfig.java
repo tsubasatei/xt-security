1769262e5b2256c915f5b1520813a3f4bda1059a
tsubasatei <tsubasa_337283708@qq.com>
2019-11-25 15:06:30 +0800

    security原理及自定义用户认证

---
+++ xt-security-browser/src/main/java/com/xt/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.xt.security.browser;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 加密
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        /**
+         * 任何方法都验证
+         */
+        http.formLogin()
+//        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
---
+++ xt-security-browser/src/main/java/com/xt/security/browser/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.xt.security.browser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+    private Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("登录用户名：" + username);
+
+        /**
+         * 根据用户名查找用户信息
+         * 根据查找到的用户信息判断用户是否被冻结
+         */
+        String encode = passwordEncoder.encode("123456");
+        logger.info("数据库的密码：" + encode);
+        return new User(username, encode,
+                true, true, true, true,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
